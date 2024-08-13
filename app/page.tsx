f9c5caf158f722d1f7fe76f9edb8a3571b4d405a
Shoxruh <shxxx404@gmail.com>
2024-08-13 22:18:35 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import NewsletterForm from "@/components/NewsletterForm";
+
+export default function Home() {
+  return (
+    <main className="bg-[#03040B] flex flex-col justify-center items-center p-10 min-h-screen">
+      <div className="space-y-1">
+        <h2 className="z-10 text-3xl font-bold text-center text-transparent duration-1000 bg-white cursor-default text-stroke animate-title sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text">
+          Join the waitlist for my
+        </h2>
+        <h1 className="z-10 text-4xl font-bold text-center text-transparent duration-1000 bg-white cursor-default sm:text-6xl md:text-7xl whitespace-nowrap bg-clip-text bg-gradient-to-r from-purple-300 to-purple-800 animate-fade-in-3">
+          Newsletter
+        </h1>
+      </div>
+      <NewsletterForm />
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,82 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        "outline-gray-focus": "inset 0 0 0 1.5px #222127",
+        "outline-gray": "inset 0 0 0 1.5px #131317",
+      },
+      animation: {
+        title: "title 3s ease-out forwards",
+        "fade-in": "fade-in 0.5s ease-in-out forwards",
+        "fade-in-3": "fade-in 3s ease-in-out forwards",
+        "fade-bottom": "fade-bottom 3s ease-in-out forwards",
+        "fade-top": "fade-top 3s ease-in-out forwards",
+      },
+      keyframes: {
+        title: {
+          "0%": {
+            "line-height": "0%",
+            "letter-spacing": "0.25rem",
+            opacity: "0",
+          },
+          "25%": {
+            "line-height": "0%",
+            opacity: "0",
+          },
+          "80%": {
+            "line-height": "100%",
+          },
+          "100%": {
+            "line-height": "100%",
+            opacity: "100%",
+          },
+        },
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          "75%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "100%",
+          },
+        },
+        "fade-in-3": {
+          "0%": {
+            opacity: "0",
+          },
+          "75%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "100%",
+          },
+        },
+        "fade-bottom": {
+          "0%": {
+            transform: "translateY(-100%)",
+            opacity: "0",
+          },
+          "30%": {
+            transform: "translateY(0%)",
+            opacity: "100%",
+          },
+          "100%": {
+            opacity: "100%",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
